cbcc36c87c0233228c382c588bc326ee59a9b8e9
quang-ute <57078914+quang-ute@users.noreply.github.com>
2023-03-27 20:22:00 +0700

    Update cookies.php

--- Hftpd_slim/apache/www/htdocs/cookies.php
+++ Hftpd_slim/apache/www/htdocs/cookies.php
@@ -11,7 +11,8 @@ if(!isset($_COOKIE[$cookie_name])) {
   echo "Cookie named '" . $cookie_name . "' is not set!";
 } else {
   echo "Cookie '" . $cookie_name . "' is set!<br>";
-  echo "Value is: " . $_COOKIE[$cookie_name];
+  echo "Value is: " . $_COOKIE[$cookie_name]."<br><br>";
+  
 }
 ?>
 
