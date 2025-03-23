11b4e4809a53171059919e98c311dde4657a93c2
quang-ute <57078914+quang-ute@users.noreply.github.com>
2025-03-23 11:45:55 +0700

    Delete Hftpd_slim directory

--- Hftpd_slim/apache/source/sniff1.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-from scapy.all import *
-pkt=sniff(iface='br-e4de50b7fb8d',filter='icmp or udp or tcp',count=20)
-pkt.summary()
--- Hftpd_slim/apache/source/tcpstream.py
+++
@@ -1,5 +0,0 @@
-from scapy.all import *
-
-scapy_cap = rdpcap('file.pcap')
-for packet in scapy_cap:
-    print (hexdump(packet[IP].payload))
