e4248a73040283821b294180ac497bc1bcd2ca99
quang-ute <57078914+quang-ute@users.noreply.github.com>
2025-03-30 20:36:36 +0700

    Delete http-lab directory

--- http-lab/apache/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Apache in the background
-service apache2 start
-
-# Keep the container running by tailing Apache logs (or another persistent process)
-tail -f /var/log/apache2/access.log
